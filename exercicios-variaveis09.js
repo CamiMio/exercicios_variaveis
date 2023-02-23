7a44b90d88e00a58bb7fe4bce5a71311d03e29ec
Camila Mio <camilamio25@gmail.com>
2023-02-22 23:24:30 -0300

    exercicio 09

---
+++ exercicios-variaveis09.js
@@ -0,0 +1,6 @@
+let lados = 6
+let soma = (lados - 2) * 180;
+let angulos = soma / lados;
+
+console.log(`a soma dos ângulos internos é ${soma}`);
+console.log( `o valor de cada ângulo é: ${angulos}`);
