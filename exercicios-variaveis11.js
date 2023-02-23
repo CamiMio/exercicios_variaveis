ea462b4c6556acc783b7d0f164e64912a00afd7e
Camila Mio <camilamio25@gmail.com>
2023-02-23 00:20:25 -0300

    exercicio 13

--- exercicios-variaveis11.js
+++ exercicios-variaveis11.js
@@ -1,3 +1,4 @@
-let raio = 3
+let diametro = 6;
+let raio = diamentro / 2;
 let volume = 4/3 * Math.PI * Math.pow(raio,3) / 3;
 console.log(`o volume de uma esfera de raio 3 é ${volume} PI`);
---
+++ exercicios-variaveis13.js
@@ -0,0 +1,4 @@
+let raio = 3;
+let altura = 1;
+let area_total = 2 * Math.PI * raio * (raio + altura) / Math.PI;
+console.log(`A area total é ${area_total} PI`);
