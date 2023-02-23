80748a499e132352307f9bf5a670426d446ec803
Camila Mio <camilamio25@gmail.com>
2023-02-22 23:12:40 -0300

    exercicio 08

---
+++ exercicios-variaveis08.js
@@ -0,0 +1,6 @@
+
+let raio = 1;
+let comprimento = 2 * Math.PI * raio;
+let area = Math.PI * Math.pow(raio,2);
+console.log(`o comprimento do círculo é : ${comprimento}`);
+console.log( `a área do círculo é ${area} `);
